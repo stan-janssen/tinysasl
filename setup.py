4eebc8f17d28fcba6700e9378db5e8714d097a93
Stan Janssen <stan@finetuned.nl>
2022-10-25 12:09:39 +0200

    Released version 0.1.1

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='tinysasl',
       description='Tiny SASL-library for Python 3 for SCRAM authentication',
       author='Stan Janssen',
-      version='0.1.0',
+      version='0.1.1',
       packages=['tinysasl'])
